0a33afa35e827ebdb994f6802c7e7c86886bb310
tangwanfa <763174066@qq.com>
2022-03-15 10:36:18 +0800

    fixed

--- app/Services/QywxMsgService.php
+++ app/Services/QywxMsgService.php
@@ -12,7 +12,14 @@ class QywxMsgService
 
     private $jsbUrl = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=629e561c-baee-4321-8f43-93867edadf10';
 
-    public function sendMsg(string $msg){
+    private $comLessonWatchBotUrl = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=58c425f7-9152-42b6-b64d-fd4880c77a02';
+
+    /**
+     * 学生信息发送到技术部群
+     * @param string $msg
+     * @return array|mixed
+     */
+    public function sendStudentMsg(string $msg){
         $data = [
             'msgtype' => 'text',
             'text' => [
@@ -20,5 +27,20 @@ class QywxMsgService
             ]
         ];
         return Http::post($this->jsbUrl,$data)->json();
+    }
+
+    /**
+     * 外教信息发送到公司群
+     * @param string $msg
+     * @return array|mixed
+     */
+    public function sendTeacherMsg(string $msg){
+        $data = [
+            'msgtype' => 'text',
+            'text' => [
+                'content' => $msg
+            ]
+        ];
+        return Http::post($this->comLessonWatchBotUrl,$data)->json();
     }
 }
